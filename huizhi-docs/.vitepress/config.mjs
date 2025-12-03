792efc70fd7df8eeb93ec255be59c87ba3d55b6c
huizhiLLL <3169164181@qq.com>
2025-12-03 17:27:49 +0800

    init

---
+++ huizhi-docs/.vitepress/config.mjs
@@ -0,0 +1,44 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Huizhi",
+  description: "Docs site for huizhi",
+  head: [
+    ['link', { rel: 'icon', href: '/public/favicon.ico' }]
+  ],
+  themeConfig: {
+    logo: '/public/favicon.ico',
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Docs', link: '/hzcubing/index' }
+    ],
+
+    sidebar: [
+      // 笔记分类
+      {
+        text: '会枝 Cubing',
+        collapsed: false,
+        items: [
+          { text: '更新日志', link: '/hzcubing/changelog' }
+        ]
+      },
+
+      // 示例分类
+      {
+        text: 'Examples',
+        collapsed: false,
+        items: [
+          { text: 'Markdown Examples', link: '/example-docs/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/example-docs/api-examples' },
+        ]
+      },
+      
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/huizhiLLL' }
+    ]
+  }
+})
---
+++ huizhi-docs/index.md
@@ -0,0 +1,25 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "Huizhi"
+  text: "A VitePress Site"
+  tagline: Some docs and notes.
+  actions:
+    - theme: brand
+      text: 会枝 Cubing
+      link: /hzcubing/index
+    - theme: alt
+      text: API Examples
+      link: /api-examples
+
+features:
+  - title: 会枝 Cubing
+    details: 魔方群网站！
+  - title: Feature B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
